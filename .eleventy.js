01373c2b89b23578edb9688bd605633fb48086b9
Raul Tavares <rtavares@gmail.com>
2022-09-11 23:07:02 +0100

    change - EDN-01 - Adjust content and styling.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     // 11ty configuration
     // Copy images to build site.
     eleventyConfig.addPassthroughCopy("endodiabnut2022/website/site-content//**/*.png")
+    eleventyConfig.addPassthroughCopy("endodiabnut2022/website/site-content//**/*.jpg")
 
     return {
         dir: {
