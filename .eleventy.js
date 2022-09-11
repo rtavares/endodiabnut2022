560e29e4f98af8d5f890bd013b7515e2ac6d6e36
Raul Tavares <rtavares@gmail.com>
2022-09-11 21:28:50 +0100

    feature - EDN-01 - Add support for images in content.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function(eleventyConfig) {
+    // 11ty configuration
+    // Copy images to build site.
+    eleventyConfig.addPassthroughCopy("endodiabnut2022/website/site-content//**/*.png")
+
     return {
         dir: {
             input: "endodiabnut2022/website/site-content/"
